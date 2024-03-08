cd6a90c1307eb4e7001b5b9f52d3dfa2f8aa73a7
mostafa kandel <101183814+MostafaKandel@users.noreply.github.com>
2024-03-08 10:46:46 +0100

    implementieren of function 'get_df'

--- aufgabe/urls.py
+++ aufgabe/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns =[
 
     path('', views.index , name='index'),
-    
+    path('data', views.get_df, name='data'),
 ]
--- aufgabe/views.py
+++ aufgabe/views.py
@@ -11,7 +11,21 @@ def index(request):
     return render(request, 'aufgabe/index.html')
     
 
-
+def get_df(request):
+    '''
+    usage: this function is used to return json data from data frame which is two columns and 1000 rows
+    returns: json data
+    '''
+    # create the random numbers for column a
+    random_numbers = np.random.randint(0, 101, size=1000)
+    #create column b
+    column_b = random_numbers % 10
+    # Create DataFrame
+    df = pd.DataFrame({'a': random_numbers, 'b': column_b})
+    # Convert DataFrame to JSON
+    data_json = df.to_json(orient='records')
+    # Return the JSON response
+    return JsonResponse(data_json, safe=False)
 
 
     '''
